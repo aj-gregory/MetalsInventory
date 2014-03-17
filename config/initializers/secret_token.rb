edd89e069f3906a18ba2a4f928bc0371d0f1254f
AJ Gregory <bertie458@gmail.com>
2014-03-17 14:03:43 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MetalsInventory::Application.config.secret_token = '5e1e05ca2a70642900e21d9100b0a80a76bdb93ae16d41a4d90d5507bdb6223853d3273d25e52ed93098d1a5d33f34ea88bbece2123c9a296de378bb082d7cc9'
