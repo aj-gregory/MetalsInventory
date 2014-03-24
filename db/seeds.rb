c4cb3384b08269e73d555f416a2a91832ed9d2f0
AJ Gregory <bertie458@gmail.com>
2014-03-23 17:26:29 -0700

    goods display in table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create!({:email => 'demo@demo.demo', :password => 'password'})
+
+warehouse = Inventory.create!({:name => 'Warehouse', :inventory_manager_id => user.id })
+                               
+store = Inventory.create!({:name => 'Store', :inventory_manager_id => user.id })
+
+goods = Good.create!([{:metal_type => 'Sheet', :material => 'Steel', :grade => '1025', :gauge => 9, :inventory_id => store.id},
+                      {:metal_type => 'Tube', :material => 'Copper', :diameter => 1, :grade => 'C1300', :length => 4, :inventory_id => store.id},
+                      {:metal_type => 'Plate', :material => 'Steel', :grade => 'AR360', :gauge => 0, :inventory_id => warehouse.id}])
+
