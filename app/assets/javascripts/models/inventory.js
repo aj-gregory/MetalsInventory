7f7ea6eed3cb41fde9abb892a84319936981b02a
AJ Gregory <aj.gregory@baseboxsoftware.com>
2014-05-07 17:10:36 -0700

    working tests

--- app/assets/javascripts/models/inventory.js
+++ app/assets/javascripts/models/inventory.js
@@ -3,5 +3,8 @@ var attr = DS.attr;
 MetalsInventory.Inventory = DS.Model.extend({
   goods: DS.hasMany('good'),
   name: attr('string'),
-  inventory_manager_id: attr('string')
+  inventory_manager_id: attr('string'),
+  elementId: function() {
+  	return (this.get('name') + '-link');
+  }.property('id')
 });
